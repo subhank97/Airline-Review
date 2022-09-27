fb444cb3615ad79880ea1192d1c68ab57bf741b4
Subhan Khan <subhank1997@gmail.com>
2022-09-27 18:32:10 -0400

    created method to take airline name and turn it into url format and method to get average score of airline review

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,11 @@
 class Airline < ApplicationRecord
     has_many :reviews
+
+    def slugify
+        self.slug = name.parameterize
+    end
+
+    def avg_score
+        reviews.average(:score).round(2).to_f
+    end
 end
